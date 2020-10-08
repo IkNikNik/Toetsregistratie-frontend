1c6c9515bfc60f9dc364af990cc14c103e39882e
Nikki <nikki.westenborg@student.hu.nl>
2020-10-08 17:01:21 +0200

    invoer knoppen toegevoegd op de algemene paginas en close buttons op de invoerpaginas.

--- Script/student.js
+++ Script/student.js
@@ -1,28 +1,111 @@
-/* Latest compiled and minified JavaScript included as External Resource */
+window.onload = function () {
+    // Stuurt fetch naar de 'cijfer' API omdat hierin de FK's in staan
+    fetch('http://62.251.126.253:63231/api/student.json')
+        .then(response => response.json())
+        .then(data => {
+            let sortVoornaam = 'ascending';
+            let sortAchternaam = 'ascending';
+            let sortNummer = 'ascending';
+            appendData(data)
 
-$(document).ready(function () {
-    $('.editBtn').click(function () {
-        if ($('.editField').is('[readonly]')) { //checks if it is already on readonly mode
-            $('.editField').prop('readonly', false);//turns the readonly off
-            $('.editBtn').html('Edit On'); //Changes the text of the button
-            $('.editBtn').css("background", "green"); //changes the background of the button
-            $('.editBtn').css("border", "green"); //changes the border of the button
-        } else { //else we do other things
-            $('.editField').prop('readonly', true);
-            $('.editBtn').html('Edit Off');
-            $('.editBtn').css("background", "red");
-        }
-    });
 
-});
+            function appendData(sortedData) {
+                let mainContainer = document.getElementById("vulling");
+                for (let i = 0; i < sortedData.length; i++) {
+                    let tr = document.createElement("tr");
+                    tr.innerHTML = '';
+                    mainContainer.appendChild(tr);
+                    let trNaam = document.createElement("td");
+                    trNaam.innerHTML = sortedData[i].voornaam;
+                    mainContainer.appendChild(trNaam);
+                    let trAchternaam = document.createElement("td");
+                    trAchternaam.innerHTML = sortedData[i].achternaam;
+                    mainContainer.appendChild(trAchternaam);
+                    let trNummer = document.createElement("td");
+                    trNummer.innerHTML = sortedData[i].student_nummer;
+                    mainContainer.appendChild(trNummer);
+                }
 
-// tell the embed parent frame the height of the content
-if (window.parent && window.parent.parent) {
-    window.parent.parent.postMessage(["resultsFrame", {
-        height: document.body.getBoundingClientRect().height,
-        slug: "w4ynU"
-    }], "*")
-}
+                document.getElementById("voornaam").onclick = function () {
+                    const gevuldeTabel = document.getElementById('vulling');
+                    gevuldeTabel.innerHTML = '';
+                    sorterenVoornaam()
+                    if (sortVoornaam === 'ascending') {
+                        sortVoornaam = 'descending';
+                    } else {
+                        sortVoornaam = 'ascending';
+                    }
+                };
 
-// always overwrite window.name, in case users try to set it manually
-window.name = "result"
+                document.getElementById("achternaam").onclick = function () {
+                    const gevuldeTabel = document.getElementById('vulling');
+                    gevuldeTabel.innerHTML = '';
+                    sorterenAchternaam()
+                    if (sortAchternaam === 'ascending') {
+                        sortAchternaam = 'descending';
+                    } else {
+                        sortAchternaam = 'ascending';
+                    }
+                };
+
+                document.getElementById("nummer").onclick = function () {
+                    const gevuldeTabel = document.getElementById('vulling');
+                    gevuldeTabel.innerHTML = '';
+                    sorterenNummer()
+                    if (sortNummer === 'ascending') {
+                        sortNummer = 'descending';
+                    } else {
+                        sortNummer = 'ascending';
+                    }
+                };
+            }
+
+            function sorterenVoornaam() {
+                const sortedVoornaam = data.sort((a, b) => {
+                    if (a.voornaam < b.voornaam) {
+                        return sortVoornaam === 'ascending' ? -1 : 1
+                    } else if (a.voornaam > b.voornaam) {
+                        return sortVoornaam === 'ascending' ? 1 : -1
+                    } else {
+                        return 0
+                    }
+
+                })
+
+                appendData(sortedVoornaam)
+            }
+
+            function sorterenAchternaam() {
+                const sortedAchternaam = data.sort((a, b) => {
+                    if (a.achternaam < b.achternaam) {
+                        return sortAchternaam === 'ascending' ? -1 : 1
+                    } else if (a.achternaam > b.achternaam) {
+                        return sortAchternaam === 'ascending' ? 1 : -1
+                    } else {
+                        return 0
+                    }
+
+                })
+
+                appendData(sortedAchternaam)
+            }
+
+            function sorterenNummer() {
+                const sortedNummer = data.sort((a, b) => {
+                    if (a.student_nummer < b.student_nummer) {
+                        return sortNummer === 'ascending' ? -1 : 1
+                    } else if (a.student_nummer > b.student_nummer) {
+                        return sortNummer === 'ascending' ? 1 : -1
+                    } else {
+                        return 0
+                    }
+
+                })
+
+                appendData(sortedNummer)
+            }
+
+        })
+        .catch((err) => console.log(err));
+};
+
